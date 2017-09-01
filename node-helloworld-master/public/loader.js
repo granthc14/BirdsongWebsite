c9b7e92358ba30aad3fbdff267b19a2cc7c554a9
Grant Cordle <you@example.com>
2017-09-01 13:34:21 -0500

    Added Pages
    - Added the pages for the rest of the tabs at the top
    - Wrote the thing that will switch the content

---
+++ node-helloworld-master/public/loader.js
@@ -0,0 +1,19 @@
+function nowPlaying() {
+    $("#content").load("nowplaying.html");
+}
+
+function comingSoon() {
+    $("#content").load("comingSoon.html");
+}
+
+function ticketInfo() {
+    $("#content").load("ticketInfo.html");
+}
+
+function concessions() {
+    $("#content").load("concessions.html");
+}
+
+function contactUs() {
+    $("#content").load("contactUs.html");
+}
