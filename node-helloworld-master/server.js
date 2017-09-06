2d9d3c3e17caeebc3d3058c9df2b0dfb563c2860
Grant Cordle <you@example.com>
2017-09-06 09:13:29 -0500

    Added new structure
    - Added new structure
    - removed old structure

--- node-helloworld-master/server.js
+++ node-helloworld-master/server.js
@@ -5,10 +5,51 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/sayHello", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Wow! " + user_name + "!");
-});
+app.get('/', function (req, res) {
+	console.log("hello, everyone"); 
+  res.send('Hello World')
+})
+
+
+//Make the api for the application
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+
+
+
+
+
+
+
 
 app.listen(port);
 console.log("Listening on port ", port);
